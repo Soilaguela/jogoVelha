272d6462b919b1b98b29c11c6fea4eda2fcfa255
Soilaguela <soilabenguela@gmail.com>
2024-03-26 21:30:12 +0000

    Update script.js
    Criação das funções:
    - getCurrentPlayer(): Função é responsável por renderizar o tabuleiro do jogo. Ela limpa o conteúdo atual do tabuleiro e, em seguida, itera sobre o gameState, criando uma <div> para cada célula do tabuleiro. Cada célula é configurada com um ouvinte de evento de clique para que os jogadores possam interagir com elas.
    
    - gameClick(): Esta função é chamada quando uma célula do tabuleiro é clicada. Ela verifica se a célula está vazia e se o jogo não acabou. Se ambas as condições forem verdadeiras, a célula é preenchida com o símbolo do jogador atual, o tabuleiro é renderizado novamente e o jogo verifica se há um vencedor ou empate.

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const board = document.querySelector('.board');
 let currentPlayer = 'X';
 let gameState = ['', '', '', '', '', '', '', '', '']; // 0,1,2,
+
+function getCurrentPlayer() {
+    board.innerHTML = '';
+    gameState.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        cellElement.innerText = cell;
+        cellElement.addEventListener('click', () => gameClick(index));
+        board.appendChild(cellElement);
+    });
+
+}
+
+function gameClick(index) {
+    if (gameState[index] === '' && gameOver()) {
+        gameState[index] = currentPlayer;
+        getCurrentPlayer();
+
+        if (checkWinner(currentPlayer)) {
+            alert('Jagador ${currentPlayer} winner!');
+            resetGame();
+
+        } else if (checkDraw) {
+            alert("It's a draw!");
+            resetGame();
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
